d12ea8d68c4692caa207422466f2f72980f2a1c2
Alexander Vinnik <v1nnik.ae@yandex.ru>
2023-05-03 21:06:05 +0700

    feat: add tests for PublicEventController

--- main/src/test/java/ru/practicum/main/controller/publicapi/PublicEventControllerSpec.groovy
+++ main/src/test/java/ru/practicum/main/controller/publicapi/PublicEventControllerSpec.groovy
@@ -1,18 +1,65 @@
 package ru.practicum.main.controller.publicapi
 
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import ru.practicum.main.dto.event.EventSearchFilter
+import ru.practicum.main.exception.EwmNotFoundException
+import ru.practicum.main.handler.MainServiceHandler
+import ru.practicum.main.mapper.category.CategoryMapper
+import ru.practicum.main.mapper.event.EventMapper
+import ru.practicum.main.model.category.Category
+import ru.practicum.main.service.category.CategoryService
+import ru.practicum.main.service.event.EventService
 import spock.lang.Specification
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 class PublicEventControllerSpec extends Specification {
 
     def "Should return 200 when get all events"() {
-        //TODO: IMPLEMENT ME!!!
-    }
+        given:
+        def service = Mock(EventService)
+        def controller = new PublicEventController(service, Mock(EventMapper))
+        def server = MockMvcBuilders
+                .standaloneSetup(controller)
+                .setControllerAdvice(MainServiceHandler)
+                .build()
 
-    def "Should return 400 when request is incorrect"() {
-        //TODO: IMPLEMENT ME!!!
+        when:
+        def request = get("/events")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+        and:
+        server.perform(request)
+                .andExpect(status().isOk())
+        then:
+        interaction {
+            1 * service.getAllEvents(EventSearchFilter.builder().build(), "/events", _ as String)
+        }
     }
 
     def "Should return 404 when event not found"() {
-        //TODO: IMPLEMENT ME!!!
+        given:
+        def service = Mock(EventService)
+        def controller = new PublicEventController(service, Mock(EventMapper))
+        def server = MockMvcBuilders
+                .standaloneSetup(controller)
+                .setControllerAdvice(MainServiceHandler)
+                .build()
+
+        when:
+        def request = get("/events/1")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+        and:
+        server.perform(request)
+                .andExpect(status().isNotFound())
+        then:
+        interaction {
+            1 * service.getEventByIdPublic(1L, "/events/1", _ as String) >>
+                    {
+                        throw new EwmNotFoundException("")
+                    }
+        }
     }
 }
