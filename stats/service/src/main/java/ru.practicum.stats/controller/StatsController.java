1b3a7ae767184f4b083586771a4245c73356552d
Alexander Vinnik <v1nnik.ae@yandex.ru>
2023-05-09 15:08:54 +0700

    refactor: Add Json DateTimeFormat for LocalDateTime in StatsService

--- stats/service/src/main/java/ru.practicum.stats/controller/StatsController.java
+++ stats/service/src/main/java/ru.practicum.stats/controller/StatsController.java
@@ -2,6 +2,7 @@ package ru.practicum.stats.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import ru.practicum.stats.dto.ViewStatsDto;
 import ru.practicum.stats.mapper.StatsMapper;
 import ru.practicum.stats.service.StatsService;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j(topic = "Stats Controller")
@@ -23,21 +25,20 @@ public class StatsController {
     private final DateTimeConverter dateTimeConverter;
 
     @PostMapping("/hit")
-    public ResponseEntity<Object> create(@RequestBody EndpointHitDto dto) {
+    public ResponseEntity<Void> create(@RequestBody EndpointHitDto dto) {
         log.info("Add EndpointHit with data: " + dto);
         statsService.create(statsMapper.toEndpointHit(dto));
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/stats")
-    public List<ViewStatsDto> getStats(@RequestParam String start,
-                                       @RequestParam String end,
+    public List<ViewStatsDto> getStats(@RequestParam
+                                       @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                       @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
                                        @RequestParam(required = false) List<String> uris,
                                        @RequestParam(defaultValue = "false") Boolean unique) {
         log.info("Get stats with values: start = {} end = {} uris = {} unique = {}", start, end, uris, unique);
         return statsMapper.toListViewStatsDto(
-                statsService.getStats(
-                        dateTimeConverter.parseDate(start),
-                        dateTimeConverter.parseDate(end), uris, unique));
+                statsService.getStats(start, end, uris, unique));
     }
 }
