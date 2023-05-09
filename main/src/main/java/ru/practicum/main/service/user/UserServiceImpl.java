8cd1442fc0ca186f15de490fc2214f43835e4a35
Alexander Vinnik <v1nnik.ae@yandex.ru>
2023-05-09 16:36:02 +0700

    fix: fix checkstyle

--- main/src/main/java/ru/practicum/main/service/user/UserServiceImpl.java
+++ main/src/main/java/ru/practicum/main/service/user/UserServiceImpl.java
@@ -2,7 +2,6 @@ package ru.practicum.main.service.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.NonUniqueObjectException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
